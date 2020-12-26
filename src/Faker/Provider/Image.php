9be0b34db20448229b1473e2738479bf9ae5ce15
seka1 <2714877+seka19@users.noreply.github.com>
2020-12-26 21:08:00 +0100

    feat: ignore invalid SSL-certs

--- src/Faker/Provider/Image.php
+++ src/Faker/Provider/Image.php
@@ -110,6 +110,7 @@ class Image extends Base
             $fp = fopen($filepath, 'w');
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_FILE, $fp);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             $success = curl_exec($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
             fclose($fp);
             curl_close($ch);
